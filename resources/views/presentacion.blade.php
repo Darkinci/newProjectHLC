92f40a13f8c9e85594455e39e7a92ae367cd4a30
Darkinci <jorge.ortega.nieto@gmail.com>
2019-10-11 14:38:54 +0200

    Mejorando la plantilla de Laravel

--- resources/views/presentacion.blade.php
+++ resources/views/presentacion.blade.php
@@ -14,7 +14,7 @@
             html, body {
                 background-color: lightskyblue;
                 color: #636b6f;
-                font-family: 'Consolas', sans-serif;
+                font-family: 'Consolas';
                 font-weight: 200;
                 height: 100vh;
                 margin: 0;
@@ -29,14 +29,32 @@
                 text-align: center;
                 color: red;
             }
+
+            p{
+                color: black;
+                text-align: center;
+            }
+
+            header{
+                width: auto;
+                height: 15%;
+                color: black;
+                border: 2px;
+            }
         </style>
     </head>
     <body>
+        <header>
+            <p> Esto es un magnifico header </p>
+        </header>
         <h1> Prueba de una plantilla de vistas en LARAVEL! </h1>
         <h2> Hecho por Jorge Ortega Nieto </h2>
         <?php 
             echo "<h3>Hoy es ". date('l jS \of F Y h:i A') . "</h3>" ;
-
         ?>
+
+        <footer>
+             <p> Hecho por Jorge </p> 
+        </footer>
     </body>
 </html>
